632bb184e4c7236b9d7fbda45fbc078b874af6ad
CarlosHenriqueGullichScherer <carloshgscherer@gmail.com>
2025-09-03 11:45:50 -0300

    Esboço index.hpp
    Escrevendo nomes possíveis funções para implementar as dinâmicas do trabalho e leve organização dos arquivos

---
+++ GerenciamentoMemoria/index.hpp
@@ -0,0 +1,22 @@
+#ifndef INDEX_HPP
+#define INDEX_HPP
+
+
+namespace gerenMemoria{
+
+void resetar();
+void sair();
+void modoDeOperação();
+void calcularFragmentação();
+void espacoLivre();
+void fragmentosDisponiveis();
+void fragmentacaoInterna();
+void fragmentacaoExterna();
+void taxaSucessoAlocacao();
+void framesLivres();
+void usoMemoria();
+void menuInterativo();
+
+}
+
+#endif 
