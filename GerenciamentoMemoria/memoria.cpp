614cf233add699538ea513a4b567ec293dcd0864
CarlosHenriqueGullichScherer <carloshgscherer@gmail.com>
2025-09-16 15:36:36 -0300

    Paginação menu completo
    páginação completa e funcional com menu interativo, código comentado

--- GerenciamentoMemoria/memoria.cpp
+++ GerenciamentoMemoria/memoria.cpp
@@ -8,10 +8,12 @@
 
 namespace gerenMemoria {
 
-    bool ehValido(const std::string &s){
-        if(s.empty()) return true;
+    bool ehValido(const std::string &s){ //função feita para validar entradas em loops, nome ehInvalido faria mais sentido
+        if(s.empty()) return true; //só aceita números inteiros maiores que 0
 
        if(s[0] == '-') return true;
+
+       if(s[0] == '0') return true;
 
         for(int i = 0; i < (int)s.size(); i++){
             if(!isdigit(s[i])) return true;
@@ -19,17 +21,17 @@ namespace gerenMemoria {
         return false;
     }
   
-    int pedirTamanhoPagina(){
+    int pedirTamanhoPagina(){ //valida e pede tamanho da página
         std::string tamanhoPagina;
         do{
             std::cout <<"Digite o tamanho da página em KB: ";     
             std::getline(std::cin, tamanhoPagina);
-        } while (ehValido(tamanhoPagina));
+        } while (ehValido(tamanhoPagina));  //não para de pedir até a entrada ser válida
         
         return std::stoi(tamanhoPagina);
     }
 
-    int pedirNumeroFrames(){
+    int pedirNumeroFrames(){ //valida e pede número de frames
         std::string nFrames;
         do{
             std::cout <<"Digite o número de frames: ";
@@ -41,23 +43,44 @@ namespace gerenMemoria {
 
     void desenharFramesVazios(std::vector<std::string>& frames){
         for(auto& frame : frames){
-            frame = "[ ]";
+            frame = "[ ]"; //desenha a memória com cada frame sendo um conchete, salvo em  um vetor de strings
         }
     }
 
-    void inserirProcesso(int &contador, int tamanhoDaPagina, std::vector<processo>& processos){
+    void inserirProcesso(int &contador, int tamanhoDaPagina,  std::vector<std::string> frames, std::vector<processo>& processos){
         std::string tamanhoDoProcesso;
         std::cout <<"Você está prestes a inserir um processo!" << std::endl;    
-
-        do{
-            std::cout << "Digite o tamanho total em KB que o processo oucupará: ";
+        int totalMemoriaLivre;
+        int frameslivres = 0;
+        for(size_t i = 0; i < frames.size(); i++){
+            if(frames[i] == "[ ]")
+                frameslivres++; //descobre quantos frames livres a memória tem
+        }
+        if(frameslivres == 0){
+            std::cout << "Você não pode inserir nada!, não há frames livres " << std::endl;
+            return; //se não há frames livres, não se pode inserir nada
+        }
+        totalMemoriaLivre = tamanhoDaPagina * frameslivres; // calcula o total de memória livre para permitir inserir processos ou não
+        do{ // se mantém nesse loop até a entrada ser válida (int) e ao mesmo tempo ser um valor que cabe na memória
+        std::cout <<"Digite o tamanho da página em KB: ";
+        std::getline(std::cin, tamanhoDoProcesso);
+        while (ehValido(tamanhoDoProcesso)){
+            std::cout << "Digite uma entrada válida: ";
             std::getline(std::cin, tamanhoDoProcesso);
-        } while (ehValido(tamanhoDoProcesso));
+        }
+        
+        
+            if (std::stoi(tamanhoDoProcesso) > totalMemoriaLivre){
+                std::cout << "A memória não suporta um processo desse tamanho no momento" << std::endl;
+            }
+            
+        } while (std::stoi(tamanhoDoProcesso) > totalMemoriaLivre || ehValido(tamanhoDoProcesso));
         
 
-        std::cout << "O processo que você inseriu terá o ID " << contador << std::endl;
+        std::cout << "O processo que você inseriu terá o ID " << contador << std::endl; //todo processo inserido recebe o id 1, e é incrementado,
+        //usuário apenas escolhe o tamanho total do processo
         
-        processo atual;
+        processo atual; //a partir do tamanho do processo já alimenta a struct do processo com o que pode ser deduzido
         atual.id = contador;
         atual.nPaginas = std::ceil((double)std::stoi(tamanhoDoProcesso) / tamanhoDaPagina);
         atual.tamanhoTotal = std::stoi(tamanhoDoProcesso);
@@ -69,57 +92,71 @@ namespace gerenMemoria {
         }
         contador++;
 
-        processos.push_back(atual);
+        processos.push_back(atual); //coloca o processo no vector dos processos em memória
     }
 
     void alocarProcesso(std::vector<std::string>& frames, int id, std::vector<processo>& processos){
-        int contador = processos.back().nPaginas;    
-        for(auto& frame : frames){
-                if(frame == "[ ]"){
+        int contador = processos.back().nPaginas;    //sempre é chamada pelo programa após a função inserir processo, por isso do back()
+        for(auto& frame : frames){ //procura frames vazios e desenha o id do processo neles [ ] -- > [1]
+                if(frame == "[ ]"){ 
                     frame = "[";
                     frame += std::to_string(id);
                     frame += "]";
                     contador--;
-                    if(contador == 0) break;
+                    if(contador == 0) break; //para quando colocar o número de páginas que o processo precisa
                 }
             }
     }
 
-    void removerProcesso(std::vector<std::string>& frames, int id, std::vector<processo>& processos){
+    void removerProcesso(std::vector<std::string>& frames, int idProcurado, std::vector<processo>& processos){
+        int posicao = 0;
         std::string alvo = "[";
-        alvo += std::to_string(id);
+        alvo += std::to_string(idProcurado);
         alvo += "]";
-        for(auto & frame : frames){
+        for(auto & frame : frames){ //desenha os frames que o processo estava ocupando como frames livres
             if(frame == alvo){
                 frame = "[ ]";
             }
         }
+        for(processo* p = processos.data(); p < processos.data() + processos.size(); p++){ //retira o processo do vetor de processos em memória
+            if(p->id == idProcurado){
+                processos.erase(processos.begin() + posicao); 
+            }
+            posicao++;
+        }
     }
 
     void exibirTabelaPaginas(std::vector<std::string> const & frames, int id, std::vector<processo>& processos){
-        int ID = processos.back().id;
         std::string alvo = "[";
-        alvo += std::to_string(ID);
+        alvo += std::to_string(id);
         alvo += "]";
         int contador = 0;
-        for(size_t i = 0; i < frames.size(); i++){
-            if(frames[i] == alvo){
-                processos.back().tabelaDePaginas[contador] = i;
+        for(size_t i = 0; i < frames.size(); i++){ //percorre a memória e descobre se os frames que o processo está ocupando
+            if(frames[i] == alvo){ //se o frame contém o id alvo
+                
+                //estou sempre armazenando o tabela de páginas num último processo inserido, isso a princípio não faz sentido,
+                //porém não altera o funcionamento da didática do programa, já que a tabela é sempre mapeada e exibida
+                //mais fácil de implementar
+                
+                processos.back().tabelaDePaginas[contador] = i; //contador (página) corresponde ao indice do vetor (frame)
                 contador++;
             }
         }
-        std::cout << "Página    " << "Frame" << std::endl;
-        for(auto &[chave, valor] : processos.back().tabelaDePaginas){
+        std::cout << "Página    " << "Frame" << std::endl; //exibe a página chave e o frame valor correspondentes
+        for(auto &par: processos.back().tabelaDePaginas){
+            auto chave = par.first;
+            auto valor = par.second;
             std::cout <<  chave << "            " << valor << std::endl;
         }
+        processos.back().tabelaDePaginas.clear(); //limpa a tabela para próxima busca não ter colisão, já que eu sempre armazeno no último
     }
 
-    void desenharFrames(std::vector<std::string>const & frames, std::vector<processo> & processos){
-        int contador = 0;
+    void desenharFrames(std::vector<std::string>const & frames){ //simplesmente exibe a memória como ela está, exatamente com os processos alocados e a quais frames eles estão ocupando
+        int contador = 0; 
         for(size_t i = 0; i < frames.size(); i++){
             std::cout << frames[i] << " ";
             contador ++;
-            if(contador >= 10){
+            if(contador >= 10){ // a cada dez frames gera uma nova linha para melhor vizualização
                 contador = 0;
                 std::cout << std::endl;
             }
@@ -128,30 +165,41 @@ namespace gerenMemoria {
 
     double calcularFragmentacao(std::vector<processo>const & processos, int nframes, int tpagina){
         double fragmentacao = 0;
-        for(auto p : processos)
-            fragmentacao += p.fragmentacao;
-
-            return  (fragmentacao / (double)(nframes * tpagina)) * 100;  
+        for(auto p : processos){
+            fragmentacao += p.fragmentacao; //acumula os bits não utilizados de todos os processos
+        }
+            return  (fragmentacao / (double)(nframes * tpagina)) * 100;   //calcula a fragmentação total em porcentagem
 
     }
 
-    double calcularFragmentacaoEspeci(processo processo, int tpagina, int id){
-        return processo.fragmentacao / processo.tamanhoTotal * 100;
+    std::string calcularFragmentacaoEspeci(std::vector<processo>& processos, int tpagina, int idProcurado){
+        processo processoLocal;
+        double fragTotal;
+        double fragPagina;
+        for (auto processo : processos){
+            if (processo.id == idProcurado){
+                processoLocal = processo; //descobre o processo que o usuário quer ver a fragmentação a partir do id passado
+            }
+        }
+        fragTotal = (double) processoLocal.fragmentacao / processoLocal.tamanhoTotal * 100; //calcula a fragmentação em relação ao processo
+        fragPagina = (double) processoLocal.fragmentacao / tpagina * 100; // em relação a última página
+        return "Relativa ao processo: " + std::to_string(fragTotal) + "% " "\n" + "Relativa a última pag " + std::to_string(fragPagina) + "%";
+       
     }
 
-    int acharProcesso(std::vector<processo>&processos){
+    int acharProcesso(std::vector<processo>&processos){ //na verdade acha o id do processo, as outras funções acabam achando a struct completa se precisarem como na função acima
         std::string id;
-        std::string ids;
-        std::string ids2;
         bool entradaInvalida = true;
-        do{
+        do{ //mantém em loop até o usuário digitar um Int e ao mesmo tempo um dos ids disponíveis
         std::cout << "Qual processo você quer selecionar(ID): " << std::endl;
+        std::string ids;
         for(auto& processo : processos){
             ids += std::to_string(processo.id) + " ";
-            ids2 += std::to_string(processo.id);
         }
-        std::cout << ids << std::endl;
+        std::cout << "Ids: disponíveis: " << ids << std::endl;
         std::getline(std::cin, id);
+        ids = "";
+        if(ehValido(id)) continue;
         for(auto& processo : processos){
             if(processo.id == std::stoi(id)){
                 entradaInvalida = false;
@@ -160,35 +208,35 @@ namespace gerenMemoria {
         }
         }while (ehValido(id) || entradaInvalida);
         
-        for(processo* p = processos.data(); p < processos.data() + processos.size(); p++) {
-            if(p->id == std::stoi(id)){
-                return p->id;
-            }
-        return 0;
+       for (auto& processo : processos) { //retorna então o processo que o usuário digitou, percorrendo o vetor para ter certeza que existe
+        if (processo.id == std::stoi(id)) {
+            return processo.id;
         }
     }
+        return 0; //retorna zero caso não ache nada, não achei um caso que isso acontecesse
+    }
 
-    void menuPaginacao() {
+    void menuPaginacao() { //mantém o usuário no menu até que ele queria sair
     bool sair = false;
 
     while (!sair) {  
         std::cout << "Você está no menu da paginação, para começar uma simulação, "
                   << "você deve inserir a quantidade de frames da memória e tamanho da página" 
-                  << std::endl;
+                  << std::endl; //pede essas informações toda vez que resetar o menu
 
         int tPagina = pedirTamanhoPagina();
         int nFrames = pedirNumeroFrames();
 
-        std::vector<std::string> frames(nFrames);
-        desenharFramesVazios(frames);
-        std::vector<processo> processos;
-        int contador = 0;
+        std::vector<std::string> frames(nFrames); //representa a memória
+        desenharFramesVazios(frames); // precisa da memória desenhada vazia como [ ] em cada frame para funcionar
+        std::vector<processo> processos; //vetor para os processos em memória, guardar os dados deles
+        int contador = 1; //processo começa com o id e próximo com id 2 e assim por diante
 
         bool reiniciar = false;
 
         while (!reiniciar && !sair) {
             std::string entrada;
-            std::cout << "\nSelecione uma das opções abaixo: " << std::endl;
+            std::cout << "\nSelecione uma das opções abaixo: " << std::endl; 
             std::cout << "1. Inserir Processo " << std::endl;
             std::cout << "2. Remover Processo " << std::endl;
             std::cout << "3. Exibir tabela de páginas " << std::endl;
@@ -205,7 +253,7 @@ namespace gerenMemoria {
 
             switch (opcao) {
                 case 1: {
-                    inserirProcesso(contador, tPagina, processos);
+                    inserirProcesso(contador, tPagina, frames, processos);
                     alocarProcesso(frames, processos.back().id, processos);
                     break;
                 }
@@ -222,7 +270,7 @@ namespace gerenMemoria {
                         std::cout << "Não há nenhum processo" << std::endl;
                     break;
                 case 4:
-                    desenharFrames(frames, processos);
+                    desenharFrames(frames);
                     break;
                 case 5: {
                     if(!processos.empty()){
@@ -234,7 +282,7 @@ namespace gerenMemoria {
                 }
                 case 6:
                     if (!processos.empty()){
-                        // std::cout << calcularFragmentacaoEspeci(acharProcesso(processos), tPagina, acharProcesso(processos)) << "%" << std::endl;
+                     std::cout << calcularFragmentacaoEspeci(processos, tPagina, acharProcesso(processos));
                     }else
                         std::cout << "Não há nenhum processo" << std::endl;
                     break;
@@ -245,7 +293,7 @@ namespace gerenMemoria {
                     sair = true; // sai do programa
                     break;
                 default:
-                    std::cout << "Opção inválida!" << std::endl;
+                    std::cout << "Opção inválida!" << std::endl; //nunca cai no default, está por padrão
                     break;
             }
         }
@@ -256,8 +304,3 @@ namespace gerenMemoria {
 
 
 
-int main(){
-   gerenMemoria::menuPaginacao();
-
-   return 0;
-}
--- GerenciamentoMemoria/memoria.hpp
+++ GerenciamentoMemoria/memoria.hpp
@@ -4,11 +4,11 @@
 #include <map>
 #include <string>
 #include <vector>
-
+//namespace que implementa paginação
 namespace gerenMemoria{
 
 
-struct processo{
+struct processo{ //struct para representar dados relevantes ao processo
     int nPaginas;
     int id;
     int fragmentacao;
@@ -16,19 +16,19 @@ struct processo{
     std::map<int, int> tabelaDePaginas;
 
 };
-bool ehValido(const std::string &s);
-int pedirTamanhoPagina();
+bool ehValido(const std::string &s); //valida entradas
+int pedirTamanhoPagina(); 
 int pedirNumeroFrames();
-void desenharFramesVazios(std::vector<std::string>& frames);
-void inserirProcesso(int &contador, int tamanhoDaPagina, std::vector<processo>& processos);
-void alocarProcesso(std::vector<std::string>& frames, int id, std::vector<processo>& processos);
-void removerProcesso(std::vector<std::string>& frames, int id, std::vector<processo>& processos);
-void desenharFrames(std::vector<std::string>const & frames, std::vector<processo>const & processos);
-void exibirTabelaPaginas(std::vector<std::string> const & frames, int id, std::vector<processo>& processos);
-double calcularFragmentacao(std::vector<processo>const & processos);
-double calcularFragmentacaoEspeci(processo processo, int tpagina, int id);
-int acharProcesso(std::vector<processo>&processos);
-void menuPaginacao();
+void desenharFramesVazios(std::vector<std::string>& frames); //desenha a tabela vazia para ser preeenchida futuramente
+void inserirProcesso(int &contador, int tamanhoDaPagina,  std::vector<std::string> frames, std::vector<processo>& processos); //insere e valida processo num vetor
+void alocarProcesso(std::vector<std::string>& frames, int id, std::vector<processo>& processos); //desenha o processo na memória
+void removerProcesso(std::vector<std::string>& frames, int id, std::vector<processo>& processos); //tira o processo do vetor e da memória
+void desenharFrames(std::vector<std::string>const & frames); //exibe o estado atual da memória
+void exibirTabelaPaginas(std::vector<std::string> const & frames, int id, std::vector<processo>& processos); //mapeia e exibe a tabela de páginas
+double calcularFragmentacao(std::vector<processo>const & processos); //fragmentação ao todo
+std::string calcularFragmentacaoEspeci(std::vector<processo>& processos, int tpagina, int idProcurado); //fragmentação de um processo
+int acharProcesso(std::vector<processo>&processos); //função auxiliar
+void menuPaginacao(); 
 }
 
 #endif 
